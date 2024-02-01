d0e0d3ca35f1cdac41389b4124f2ba15a08a1071
siddhant-sri <sidz.dec26@gmail.com>
2024-02-01 22:59:05 +0530

    Ignited the app

--- App.js
+++ App.js
@@ -1,24 +1,11 @@
-/**
- *
- * <div id = "parent">
- *      <div id = "child">
- *          <h1>I'm an h1 tag</h1>
- *          <h2>I'm an h2 tag</h1>
- *      </div>
- *      <div id = "child2">
- *          <h1>I'm an h1 tag</h1>
- *          <h2>I'm an h2 tag</h1>
- *      </div>
- * </div>
- * 
- * ReactElement -> object ==(browser convert)==> HTML (Browser Understands)
- *
- */
+import React from "react"
+import ReactDOM from "react-dom"
+
 
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I'm an h1 tag"),
-    React.createElement("h2", {}, "I'm an h2 tag"),
+    React.createElement("h1", {}, "Namaste React"),
+    React.createElement("h2", {}, "From Scrat"),
   ]),
   React.createElement("div", { id: "child2" }, [
     React.createElement("h1", {}, "I'm an h1 tag"),
