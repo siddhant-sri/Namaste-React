8c50a0bc70cb53ce88f106e675c04360731eea31
siddhant-sri <sidz.dec26@gmail.com>
2024-02-03 16:08:36 +0530

    Laid the foundation

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
-import React from "react"
-import ReactDOM from "react-dom"
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// JSX (transpiled before it reaches to JS engine) - Parcel - Babel
+
+// JSX -> Babel transpiled it to React.createElement -> React Element (JS object) -> HTMLelement(render)
 
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "Namaste React"),
-    React.createElement("h2", {}, "From Scrat"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I'm an h1 tag"),
-    React.createElement("h2", {}, "I'm an h2 tag"),
-  ])
-]);
+const title2 = (
+  <div>
+    <h1>Parcel is a beast</h1>
+  </div>
+)
+
+const Title = () => <h1 id="parent">Namaste React from JSX</h1>;
+
+// React Component
+
+const HeadingComponent = () => (
+  <div id="container">
+
+    {title2}
+    {Title()}
+    <Title/>  
+    <Title></Title>
+    <h1>React Functional Component</h1>
+  </div>
+)
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-console.log(parent);  //object
-
-root.render(parent);
+root.render(<HeadingComponent/>);
