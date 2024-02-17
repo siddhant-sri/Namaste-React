18130c3dfaaca91a3aaf4daf38206991de2c992d
siddhant-sri <sidz.dec26@gmail.com>
2024-02-17 23:03:21 +0530

    fetched data from live Swiggy API and added search functionality

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,6 +1,9 @@
 import { LOGO_URL } from "../utils/constants";
+import { useState } from "react";
 
 const Header = () => {
+  const [btnName, setBtnName] = useState("Login");
+
   return (
     <div className="header">
       <div className="logo-container">
@@ -13,6 +16,14 @@ const Header = () => {
           <li>About Us</li>
           <li>Contact Us</li>
           <li>Cart</li>
+          <button
+            className="login"
+            onClick={() => {
+              btnName === "Login" ? setBtnName("Logout") : setBtnName("Login");
+            }}
+          >
+            {btnName}
+          </button>
         </ul>
       </div>
     </div>
